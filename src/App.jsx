436a8528abe8e67ae9dfec050ecc4717be4f7cd4
camdd <camila.dambrosio95@gmail.com>
2024-08-10 18:07:40 -0300

    shouldforwardprop para evitar warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
-
 import React from 'react';
-import { styleReset } from 'react95';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import Navbar from './components/Navbar/';
+import { StyleSheetManager } from 'styled-components';
+import isPropValid from '@emotion/is-prop-valid';
+import './app.css'; 
 
 /* Pick a theme of your choice */
 import peggysPastels from 'react95/dist/themes/peggysPastels';
 
-/* Original Windows95 font (optional) */
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-
-const GlobalStyles = createGlobalStyle`
-  ${styleReset}
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
-  body, input, select, textarea {
-    font-family: 'ms_sans_serif';
-  }
-`;
-
 const App = () => (
-  <div>
-    <GlobalStyles />
+<StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
     <ThemeProvider theme={peggysPastels}>
-      <Navbar/>
-
+      <Navbar />
     </ThemeProvider>
-  </div>
+  </StyleSheetManager>
 );
 
 export default App;
