b1e73aeef1194a54713bc0d1b0910e954f1c39cb
camdd <camila.dambrosio95@gmail.com>
2024-08-15 08:26:21 -0300

    creado el blog y conexión con el back

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import Layout from '../../layouts/Layout'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Layout from '../../layouts/Layout';
+import { GroupBoxDemo } from '../../components/GroupBox/GroupBox';
 
 export default function Blog() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/blog')
+      .then(res => setPosts(res.data))
+      .catch(err => console.log('Error fetching posts: ' + err));
+  }, []);
+
   return (
     <Layout> 
-        Blog
+      <h1>Posts</h1>
+      {posts.map(post => (
+        <GroupBoxDemo key={post._id} label={post.title}>
+          <p>{post.content}</p>
+          <p><strong>Date</strong> {post.author}</p>
+        </GroupBoxDemo>
+      ))}
     </Layout>
-  )
+  );
 }
